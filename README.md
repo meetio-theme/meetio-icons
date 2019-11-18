d135e978832133aabd09cbb3583326c4aa75c84d
Mauro Reis Vieira <mauroreisvieira@gmail.com>
2019-11-18 14:37:45 +0000

    fix: removed icons.

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # Meetio Theme Icons
+
+The most delicious and wonderful icons for Meetio Theme
+
+### Installation
+
+```bash
+# NPM
+$ npm install meetio-theme-icons --save
+
+# YARN
+$ yarn add meetio-theme-icons
+```
--- src/index.js
+++ src/index.js
@@ -9,7 +9,8 @@ module.exports.run = function(settings = { dist: "icons", options: {} }) {
     const { dist, options } = settings
     const sizes = { ...options, ...defaultOptions }
 
-    fs.readdirSync("./src/svg/").forEach(icon => {
+    const svgSrc = path.join(__dirname, `/../src/svg/`)
+    fs.readdirSync(svgSrc).forEach(icon => {
         const iconPath = path.join(__dirname, `/../src/svg/${icon}`)
         icon = icon
             .split(".")
